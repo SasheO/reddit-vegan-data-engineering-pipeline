b080fbf1fe48eb64e69c6bf42e2db0601418d9f2
M.S. Ojuba <76496417+SasheO@users.noreply.github.com>
2026-03-23 00:10:53 +0100

    Reorganise and clean up extract_reddit_json_to_postgres.py script

--- src/extract_reddit_json_to_postgres.py
+++ src/extract_reddit_json_to_postgres.py
@@ -37,16 +37,18 @@ def handler():
     count_of_posts_fetched = 0
     count_of_success_response = 0
 
-    
-
-    # time.sleep(600)
     response = requests.get(url, params=params)
 
     while True:
         if response.status_code == 200:
+            count_of_success_response += 1
+
+            # open up connection to database and start preamble for sql query
             conn = get_db_connection()
             cursor = conn.cursor()
-            count_of_success_response += 1
+            sql_query_to_insert_table_values = f"insert into {table_name} (post_id, created_utc, post_title, author_id, author_username, upvote_count, downvote_count, comments_count, crossposts_count, awards_received_count, post_text, post_url, has_media, media_type, media_title, media_src, media_url)  values "
+
+            # parse the json response and extract useful data
             response_json = response.json()
             pagination_after_key = response_json["data"]["after"] 
             params["after"] = pagination_after_key
@@ -54,7 +56,6 @@ def handler():
             reddit_posts = response_json["data"]["children"]
             print("number of posts fetched:", len(reddit_posts))
             count_of_posts_fetched += len(reddit_posts)
-            sql_code = f"insert into {table_name} (post_id, created_utc, post_title, author_id, author_username, upvote_count, downvote_count, comments_count, crossposts_count, awards_received_count, post_text, post_url, has_media, media_type, media_title, media_src, media_url)  values "
             for post in reddit_posts:
                 if "id" not in post["data"]:
                     post_id = ""
@@ -80,42 +81,34 @@ def handler():
                     awards_received_count = 0
                 else:
                     awards_received_count = post["data"]["total_awards_received"]
-
                 if "selftext" not in post["data"]:
                     post_text = None
                 else:
                     post_text = post["data"]["selftext"].replace(",", ";").replace("'", "").replace('"', "").replace("\n", "\t") 
-
                 if "title" not in post["data"]:
                     post_title = ""
                 else:
                     post_title = post["data"]["title"].replace(",", ";").replace("'", "").replace('"', "").replace("\n", "\t")
-
                 if "num_comments" not in post["data"]:
                     comments_count = 0
                 else:
                     comments_count = post["data"]["num_comments"]
-
                 if "created_utc" not in post["data"]:
                     created_utc = 0
                 else:
                     created_utc = post["data"]["created_utc"]
-
                 if "url" not in post["data"]:
                     url_to_post = None
                 else:
                     url_to_post = post["data"]["url"]
-
                 if "num_crossposts" not in post["data"]:
                     crossposts_count = 0
                 else:
                     crossposts_count = post["data"]["num_crossposts"]
-                    
                 if "media" not in post["data"]:
                     media_json = None 
                 else:
                     media_json = post["data"]["media"]
-
                 if media_json:
                     has_media = True
                 else:
@@ -136,46 +129,39 @@ def handler():
                     media_title = media_json["oembed"]["title"].replace(",", ";").replace("'", "").replace('"', "").replace("\n", "\t")
                 except:
                     media_title = None
-
-                                
-                # TODO: write database here
                 if has_media:
                     has_media = 1
                 else:
                     has_media = 0
                 
+                # 
                 # TODO: ensure that null values (e.g. null media urls and other like fields) are not entered as 'null' strings
-                sql_insert_value = f"('{post_id}', to_timestamp({int(created_utc)}), '{post_title}'::varchar(130), '{author_id}'::varchar(20), '{author_username}'::varchar(30), {upvote_count}, {downvote_count}, {comments_count}, {crossposts_count}, {awards_received_count}, '{post_text}'::varchar(1000), '{url_to_post}'::varchar(130), {has_media}::bit, '{media_type}'::varchar(20), '{media_title}'::varchar(130), '{media_src}'::varchar(130), '{media_url}'::varchar(130)), " # ON CONFLICT (post_id) DO NOTHING;"
-                sql_insert_value = sql_insert_value.replace("None", "null")
-                sql_code += sql_insert_value
-                print(post_id)
-
-            if count_of_success_response >= 10: # because at most ten pages can be returned from reddit non-oauth api
-                break
+                insert_value = f"('{post_id}', to_timestamp({int(created_utc)}), '{post_title}'::varchar(130), '{author_id}'::varchar(20), '{author_username}'::varchar(30), {upvote_count}, {downvote_count}, {comments_count}, {crossposts_count}, {awards_received_count}, '{post_text}'::varchar(1000), '{url_to_post}'::varchar(130), {has_media}::bit, '{media_type}'::varchar(20), '{media_title}'::varchar(130), '{media_src}'::varchar(130), '{media_url}'::varchar(130)), " # ON CONFLICT (post_id) DO NOTHING;"
+                insert_value = insert_value.replace("None", "null")
+                sql_query_to_insert_table_values += insert_value
+                # print(post_id)
             
-            response = requests.get(url, params=params)
-
-            # TODO: ensure that duplicate rows are skipped (post_id is primary key)
-            # sql_code += " ON CONFLICT (post_id) DO NOTHING;"
-            sql_code = sql_code[:-2] + "ON CONFLICT (post_id) DO NOTHING;" # in case there are duplicate rows with primary key (post_id)
-
-            # with open("sql_code.txt", "w", encoding='utf-8', errors='ignore') as f:
-            #     f.write(sql_code)
-            #     f.write("\n")
-
-            cursor.execute(sql_code)
+            # run sql query, close connection to database
+            sql_query_to_insert_table_values = sql_query_to_insert_table_values[:-2] + " ON CONFLICT (post_id) DO NOTHING;" # in case there are duplicate rows with primary key (post_id)
+            cursor.execute(sql_query_to_insert_table_values)
             conn.commit()
             cursor.close()
             conn.close()
+
+            if count_of_success_response >= 10: # because at most ten pages can be returned from reddit non-oauth api
+                break
+            else: # do it again!
+                response = requests.get(url, params=params)
         else:
+            # TODO: convert this to writing errors to log rather than print statements
             print(response.status_code, response.text)
             headers = dict(response.headers)
             print(headers)
-            print(int(headers['x-ratelimit-reset']))
             time.sleep(int(headers['x-ratelimit-reset'])+5)
             response = requests.get(url, params=params)
-
-    print("total number of posts fetched:", count_of_posts_fetched) # TODO: make this write logs to a table rather than printing
+            
+    # TODO: make this write logs to a table rather than printing
+    print("total number of posts fetched:", count_of_posts_fetched) 
     
 
 handler()
